64fd4e9d327b67ee7efca516b95b834f79e074f9
gioeleminardi <gioelem3@gmail.com>
2020-04-20 14:39:54 +0200

    added double texture mix

--- shaders/shader.frag
+++ shaders/shader.frag
@@ -6,9 +6,10 @@ in vec3 vertexColor;
 in vec2 TexCoord;
 
 uniform vec4 ourColor;
-uniform sampler2D ourTexture;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = texture(ourTexture, TexCoord) * vec4(vertexColor, 1.0f);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.4);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,8 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 
 void process_input(GLFWwindow *window);
 
-unsigned int load_texture();
+unsigned int load_texture(const char *filename);
+unsigned int load_textureA(const char *filename);
 
 int main() {
     glfwInit();
@@ -45,10 +46,10 @@ int main() {
 
     // mesh
     float vertices[] = {
-             0.5f,  0.5f, 0.0f,     1.0f, 0.0f, 0.0f,    1.0f, 1.0f, //TR
-             0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,    1.0f, 0.0f, //BR
-            -0.5f, -0.5f, 0.0f,     0.0f, 0.0f, 1.0f,    0.0f, 0.0f, //BL
-            -0.5f,  0.5f, 0.0f,     1.0f, 1.0f, 1.0f,    0.0f, 1.0f  //TL
+            0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, //TR
+            0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, //BR
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, //BL
+            -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f  //TL
     };
 
     unsigned int indices[] = {
@@ -92,9 +93,9 @@ int main() {
     // wireframe
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-
     // generate texture
-    unsigned int texture = load_texture();
+    unsigned int texture1 = load_texture("../textures/wall.jpg");
+    unsigned int texture2 = load_textureA("../textures/textureteo.png");
 
     float currentTime;
     float deltaTime;
@@ -130,10 +131,14 @@ int main() {
 
         ourShader.use();
         {
+            ourShader.setInt("texture1", 0);
+            ourShader.setInt("texture2", 1);
             ourShader.setFloat4("ourColor", 0.0f, greenValue, 0.0f, 1.0f);
 
             glActiveTexture(GL_TEXTURE0);
-            glBindTexture(GL_TEXTURE_2D, texture);
+            glBindTexture(GL_TEXTURE_2D, texture1);
+            glActiveTexture(GL_TEXTURE1);
+            glBindTexture(GL_TEXTURE_2D, texture2);
 
             glBindVertexArray(VAO);
             {
@@ -171,10 +176,10 @@ void process_input(GLFWwindow *window) {
     }
 }
 
-unsigned int load_texture() {
+unsigned int load_texture(const char *filename) {
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true);
-    unsigned char *textureData = stbi_load("../textures/container.jpg", &width, &height, &nrChannels, 0);
+    unsigned char *textureData = stbi_load(filename, &width, &height, &nrChannels, 0);
 
     unsigned int texture;
     glGenTextures(1, &texture);
@@ -186,6 +191,30 @@ unsigned int load_texture() {
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         if (textureData) {
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, textureData);
+            glGenerateMipmap(GL_TEXTURE_2D);
+        } else {
+            std::cerr << "Error loading texture\n";
+        }
+    }
+    stbi_image_free(textureData);
+    return texture;
+}
+
+unsigned int load_textureA(const char *filename) {
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char *textureData = stbi_load(filename, &width, &height, &nrChannels, 0);
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    {
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        if (textureData) {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData);
             glGenerateMipmap(GL_TEXTURE_2D);
         } else {
             std::cerr << "Error loading texture\n";
