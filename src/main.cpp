4c0fbf456e633036880f00f3684efd56295abfd8
gioeleminardi <gioelem3@gmail.com>
2020-04-27 14:00:51 +0200

    test instancing

--- src/main.cpp
+++ src/main.cpp
@@ -79,12 +79,47 @@ int main() {
 
     // Model
     //Model nanosuit("../models/nanosuit/nanosuit.obj");
-    Model sponza("../models/sponza/sponza.obj");
+    //Model sponza("../models/sponza/sponza.obj");
+
+    Model planet("../models/planet/planet.obj");
+    Model rock("../models/rock/rock.obj");
+
+
     // Shader
     Shader modelLoadingShader("../shaders/model_loading.vert", "../shaders/model_loading.frag");
 
     // wireframe
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    unsigned int amount = 3000;
+    glm::mat4 *modelMatrices;
+    modelMatrices = new glm::mat4[amount];
+    srand(glfwGetTime()); // initialize random seed
+    float radius = 50.0;
+    float offset = 2.5f;
+    for (unsigned int i = 0; i < amount; i++) {
+        glm::mat4 model = glm::mat4(1.0f);
+        // 1. translation: displace along circle with 'radius' in range [-offset, offset]
+        float angle = (float) i / (float) amount * 360.0f;
+        float displacement = (rand() % (int) (2 * offset * 100)) / 100.0f - offset;
+        float x = sin(angle) * radius + displacement;
+        displacement = (rand() % (int) (2 * offset * 100)) / 100.0f - offset;
+        float y = displacement * 0.4f; // keep height of field smaller compared to width of x and z
+        displacement = (rand() % (int) (2 * offset * 100)) / 100.0f - offset;
+        float z = cos(angle) * radius + displacement;
+        model = glm::translate(model, glm::vec3(x, y, z));
+
+        // 2. scale: scale between 0.05 and 0.25f
+        float scale = (rand() % 20) / 100.0f + 0.05;
+        model = glm::scale(model, glm::vec3(scale));
+
+        // 3. rotation: add random rotation around a (semi)randomly picked rotation axis vector
+        float rotAngle = (rand() % 360);
+        model = glm::rotate(model, rotAngle, glm::vec3(0.4f, 0.6f, 0.8f));
+
+        // 4. now add to list of matrices
+        modelMatrices[i] = model;
+    }
 
     float currentTime;
     float deltaTime;
@@ -94,8 +129,6 @@ int main() {
     int frames{};
     std::stringstream ss_fps;
     glm::vec4 clear_color(0.0f, 0.0f, 0.0f, 1.0f);
-    //float modelAngle = 0.0f;
-    //float rotatingSpeed = 40.0f;
 
     while (!glfwWindowShouldClose(window)) {
         currentTime = (float) glfwGetTime();
@@ -151,12 +184,19 @@ int main() {
         //modelAngle += rotatingSpeed * deltaTime;
         //modelAngle = glm::mod(modelAngle, 360.0f);
         //model = glm::rotate(model, glm::radians(modelAngle), glm::vec3(0.0f, -1.0f, 0.0f));
-        model = glm::scale(model, glm::vec3(0.2f));
+        //model = glm::scale(model, glm::vec3(0.2f));
 
+        // draw planet
+        model = glm::translate(model, glm::vec3(0.0f, -3.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(4.0f, 4.0f, 4.0f));
         modelLoadingShader.setMat4("model", model);
+        planet.Draw(modelLoadingShader);
 
-        //nanosuit.Draw(modelLoadingShader);
-        sponza.Draw(modelLoadingShader);
+        // draw meteorites
+        for (unsigned int i = 0; i < amount; i++) {
+            modelLoadingShader.setMat4("model", modelMatrices[i]);
+            rock.Draw(modelLoadingShader);
+        }
 
         // check and call events and swap the buffers
         glfwSwapBuffers(window);
